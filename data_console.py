45ae262331a5f8d453a5e58ae05a68e8873814b7
keomouny <mouny.keo@hetic.net>
2021-03-09 14:11:09 +0100

    create file for get data to insert in data

--- data_console.py
+++ data_console.py
@@ -1,5 +1,6 @@
 from setup_logger import logger
 
+
 class DataConsole:
 
     def __init__(self):
@@ -7,3 +8,12 @@ class DataConsole:
 
     def get_data(self):
         logger.info('instanciation DataConsole and get data')
+        return [
+            ('Amiga CD32', 1993, 'Commodore', 'Console'),
+            ('Playstation Vita', 2011, 'Sony', 'Console portable'),
+            ('Nintendo 3DS', 2011, 'Nintendo', 'Console portable'),
+            ('Wii U', 2012, 'Nintendo', 'Console'),
+            ('Neo-Geo', 1990, 'SNK', 'Console - Arcade'),
+            ('Xbox', 2001, 'Microsoft', 'Console'),
+            ('Playstation', 1994, 'Sony', 'Console'),
+        ]
