7ed1e4b3c98a96d188e31aee9cdde7068c5fa891
Ubuntu <mouny@mouny-vm.dpfb0tgqu3mupncs4eaxtgex1d.parx.internal.cloudapp.net>
2021-03-09 11:48:34 +0000

    initialize file for monitoring

---
+++ setup_logger.py
@@ -0,0 +1,6 @@
+import logging
+import os
+
+logging.basicConfig(filename=f"{os.getcwd()}/logs/log.log",filemode="a", format="%(asctime)s - %(levelname)-8s - %(message)s", datefmt="%d/%m/%Y %H:%M:%S", level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
